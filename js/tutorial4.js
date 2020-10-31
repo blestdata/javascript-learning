094a75166bcd3bb724be1b6e8c98ddfdd07f33be
blestdata <blestdata@gmail.com>
2020-10-31 14:52:00 +0530

    Data types start

---
+++ js/tutorial4.js
@@ -0,0 +1,10 @@
+/**
+ * Data types in Javascript
+ * 
+ * Two types of data types exists in Javascript:-
+ * 1. Primitive Data type.
+ * 2. Reference data type.
+ * 
+ * Primitive Data type stores data in form of Stack
+ * Reference data type stores data in form of Heap [Dynamic]
+ */
